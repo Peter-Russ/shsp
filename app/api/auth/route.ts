2c8ad18ed61976549b16e2a1253331b739c67f10
Peter-Russ <peterruss1996@gmail.com>
2023-05-06 20:22:40 +0200

    update to nextjs 13.4

---
+++ app/api/auth/route.ts
@@ -0,0 +1,4 @@
+import { connectToDatabase } from "@/lib/mongodb";
+import User from "@/models/user";
+import { NextResponse } from "next/server";
+
--- models/user.ts
+++ models/user.ts
@@ -31,6 +31,7 @@ const UserSchema = new Schema(
         type: [String],
       },
     },
+    { timestamps: true }
   );
   
 const User = models.User || model("User", UserSchema);
