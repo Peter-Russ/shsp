0cb06661a04a73a35f3df5ec810616bab0b26d01
Peter-Russ <peterruss1996@gmail.com>
2023-06-06 20:12:55 +0200

    setup finish

--- components/Cards/VideoCards.tsx
+++ components/Cards/VideoCards.tsx
@@ -14,7 +14,7 @@ export default async function Cards(video: Video) {
             </Link>
 
             <div className={style.videoData}>
-                <img className={style.profileImg} src="https://pbs.twimg.com/profile_images/1546458449390739457/yBCOrrIR_400x400.jpg" alt="" />
+                <img className={style.profileImg} src="" alt="" />
                 <div>
                     <h4>{video.title!}</h4>
                     <span>{video.description ?? ""}</span>
